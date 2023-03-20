b89026e0eff7a3766e666ac79e53fc168ff1510b
jeongboram <pink0913@nate.com>
2023-03-20 16:14:24 +0900

    feat: Loading state 추가

--- src/components/dashboard/Chart.tsx
+++ src/components/dashboard/Chart.tsx
@@ -5,6 +5,8 @@ import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recha
 import Title from './Title';
 import * as zoomApi from '@apis/index';
 import { Report } from '@mui/icons-material';
+import { CircularProgress, Grid } from '@mui/material';
+import { Box } from '@mui/system';
 
 // Generate Sales Data
 
@@ -17,13 +19,12 @@ type Report = {
 export default function Chart() {
 	const theme = useTheme();
 
-	const [isLoading, setIsLoading] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 	const [dailyReport, setDailyReport] = useState<Report | null>(null);
 	const [dataRes, setDataRes] = useState([]);
 
 	const getSample = async () => {
 		const res = await zoomApi.default.getReport('2023', '03');
-		setIsLoading(true);
 		setDailyReport(res.data);
 		let createdDatas: any = [];
 		for (let i = 0; i <= res.data?.dates.length; i++) {
@@ -45,35 +46,42 @@ export default function Chart() {
 
 	return (
 		<React.Fragment>
-			<Title>데일리 리포트 {`${dailyReport?.year}년 ${dailyReport?.month}월`}</Title>
+			{isLoading && (
+				<Box sx={{ display: 'flex' }}>
+					<CircularProgress />
+				</Box>
+			)}
 			{!isLoading && (
-				<ResponsiveContainer>
-					<LineChart
-						data={dataRes}
-						margin={{
-							top: 16,
-							right: 16,
-							bottom: 0,
-							left: 24,
-						}}
-					>
-						<XAxis dataKey="time" stroke={theme.palette.text.secondary} style={theme.typography.body2} />
-						<YAxis stroke={theme.palette.text.secondary} style={theme.typography.body2}>
-							<Label
-								angle={270}
-								position="left"
-								style={{
-									textAnchor: 'middle',
-									fill: theme.palette.text.primary,
-									...theme.typography.body1,
-								}}
-							>
-								Sales ($)
-							</Label>
-						</YAxis>
-						<Line isAnimationActive={false} type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
-					</LineChart>
-				</ResponsiveContainer>
+				<>
+					<Title>데일리 리포트 {`${dailyReport?.year}년 ${dailyReport?.month}월`}</Title>
+					<ResponsiveContainer>
+						<LineChart
+							data={dataRes}
+							margin={{
+								top: 16,
+								right: 16,
+								bottom: 0,
+								left: 24,
+							}}
+						>
+							<XAxis dataKey="time" stroke={theme.palette.text.secondary} style={theme.typography.body2} />
+							<YAxis stroke={theme.palette.text.secondary} style={theme.typography.body2}>
+								<Label
+									angle={270}
+									position="left"
+									style={{
+										textAnchor: 'middle',
+										fill: theme.palette.text.primary,
+										...theme.typography.body1,
+									}}
+								>
+									Sales ($)
+								</Label>
+							</YAxis>
+							<Line isAnimationActive={false} type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
+						</LineChart>
+					</ResponsiveContainer>
+				</>
 			)}
 		</React.Fragment>
 	);
